62c4fc125b7515c2b28c7eeb791fe8d83adc689e
Lukas Velek <lukas.velek@centrum.cz>
2024-11-15 09:25:28 +0100

    System presenter, AAdminPresenter sidebar update

--- app/modules/AdminModule/Presenters/AAdminPresenter.php
+++ app/modules/AdminModule/Presenters/AAdminPresenter.php
@@ -9,6 +9,9 @@ use App\Modules\TemplateObject;
 abstract class AAdminPresenter extends AContainerPresenter {
     private bool $isMembers;
     private bool $isDocuments;
+    private bool $isSystem;
+
+    private bool $isSidebar;
 
     private Sidebar $sidebar;
 
@@ -18,33 +21,41 @@ abstract class AAdminPresenter extends AContainerPresenter {
         $this->moduleName = 'Admin';
         $this->isMembers = false;
         $this->isDocuments = false;
+        $this->isSystem = false;
+
+        $this->isSidebar = false;
     }
 
     protected function setDocuments() {
         $this->isDocuments = true;
+        $this->isSidebar = true;
     }
 
     protected function setMembers() {
         $this->isMembers = true;
+        $this->isSidebar = true;
+    }
+
+    protected function setSystem() {
+        $this->isSystem = true;
+        $this->isSidebar = true;
     }
 
     public function startup() {
         parent::startup();
+
+        if($this->isSidebar) {
+            $this->sidebar = new Sidebar();
+        }
 
         if($this->isMembers) {
             $members = $this->checkActivePage('Members');
             $groups = $this->checkActivePage('Groups');
             $users = $this->checkActivePage('Users');
 
-            $this->sidebar = new Sidebar();
-
             $this->sidebar->addLink('Dashboard', $this->createFullURL('Admin:Members', 'dashboard'), $members);
             $this->sidebar->addLink('Groups', $this->createFullURL('Admin:Groups', 'list'), $groups);
             $this->sidebar->addLink('Users', $this->createFullURL('Admin:Users', 'list'), $users);
-
-            $this->addBeforeRenderCallback(function(TemplateObject $template) {
-                $template->sidebar = $this->sidebar;
-            });
         }
 
         if($this->isDocuments) {
@@ -52,12 +63,20 @@ abstract class AAdminPresenter extends AContainerPresenter {
             $folders = $this->checkActivePage('DocumentFolders');
             $metadata = $this->checkActivePage('DocumentMetadata');
 
-            $this->sidebar = new Sidebar();
-
             $this->sidebar->addLink('Dashboard', $this->createFullURL('Admin:Documents', 'dashboard'), $documents);
             $this->sidebar->addLink('Folders', $this->createFullURL('Admin:DocumentFolders', 'list'), $folders);
             $this->sidebar->addLink('Metadata', $this->createFullURL('Admin:DocumentMetadata', 'list'), $metadata);
+        }
 
+        if($this->isSystem) {
+            $system = $this->checkActivePage('System');
+            $transactionLog = $this->checkActivePage('TransactionLog');
+
+            $this->sidebar->addLink('Dashboard', $this->createFullURL('Admin:System', 'dashboard'), $system);
+            $this->sidebar->addLink('Transaction log', $this->createFullURL('Admin:TransactionLog', 'list'), $transactionLog);
+        }
+
+        if($this->isSidebar) {
             $this->addBeforeRenderCallback(function(TemplateObject $template) {
                 $template->sidebar = $this->sidebar;
             });

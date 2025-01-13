440325722bedbebb5473cbf48e0ca9cbda33d942
Lukas Velek <lukas.velek@centrum.cz>
2025-01-13 14:16:36 +0100

    ErrorModule creation

---
+++ app/modules/ErrorModule/ErrorModule.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Modules\ErrorModule;
+
+use App\Modules\AModule;
+
+class ErrorModule extends AModule {
+    public function __construct() {
+        parent::__construct('ErrorModule');
+    }
+}
+
+?>

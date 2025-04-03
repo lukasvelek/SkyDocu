b85090bd562ad59e92251da5414c83875630499b
Lukas Velek <lukas.velek@centrum.cz>
2025-04-03 15:21:20 +0200

    Update Configuration.php

--- app/core/Configuration.php
+++ app/core/Configuration.php
@@ -25,10 +25,11 @@ class Configuration {
      * Returns current version
      */
     public static function getCurrentVersion(): string {
+        $releaseDate = (APP_VERSION_RELEASE_DATE != '-' ? ('_' . APP_VERSION_RELEASE_DATE) : '');
         if(APP_BRANCH == 'TEST') {
-            return APP_VERSION . '+Build_' . APP_VERSION_BUILD . '+Branch_' . APP_BRANCH;
+            return APP_VERSION . '+Build_' . APP_VERSION_BUILD . $releaseDate . '+Branch_' . APP_BRANCH;
         } else {
-            return APP_VERSION . ' (' . APP_VERSION . '+Build_' . APP_VERSION_BUILD . '+Branch_' . APP_BRANCH . ')';
+            return APP_VERSION . ' (' . APP_VERSION . '+Build_' . APP_VERSION_BUILD . $releaseDate . '+Branch_' . APP_BRANCH . ')';
         }
     }
 }

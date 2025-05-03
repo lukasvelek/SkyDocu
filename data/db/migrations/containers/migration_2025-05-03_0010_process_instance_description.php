006ee1871c11ebb96eef6dfcb32688429278b951
Lukas Velek <lukas.velek@centrum.cz>
2025-05-03 13:23:14 +0200

    Migrations update

---
+++ data/db/migrations/containers/migration_2025-05-03_0010_process_instance_description.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Data\Db\Migrations\Containers;
+
+use App\Core\DB\ABaseMigration;
+use App\Core\DB\Helpers\TableSchema;
+use App\Core\DB\Helpers\TableSeeding;
+
+class migration_2025_05_03_0010_process_instance_description extends ABaseMigration {
+    public function up(): TableSchema {
+        $schema = $this->getTableSchema();
+
+        $schema->update('process_instances')
+            ->varchar('description', 256)
+            ->default('description', 'Process instance');
+
+        return $schema;
+    }
+
+    public function down(): TableSchema {
+        return $this->getTableSchema();
+    }
+
+    public function seeding(): TableSeeding {
+        return $this->getTableSeeding();
+    }
+}
+
+?>

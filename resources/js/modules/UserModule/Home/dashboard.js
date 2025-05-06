1de9c8edc38589f82fdba1a27f9a53fe899f4b21
Lukas Velek <lukas.velek@centrum.cz>
2025-05-06 19:36:28 +0200

    UserModule Home dashboard update

--- resources/js/modules/UserModule/Home/dashboard.js
+++ resources/js/modules/UserModule/Home/dashboard.js
@@ -8,18 +8,50 @@ async function loadData(_endpointUrl, _token) {
                 "instanceId",
                 "currentOfficerId",
                 "currentOfficerType",
-                "dateCreated",
                 "status",
+                "dateCreated",
                 "dateModified"
             ]
         }
     };
 
-    $.post(
+    await $.post(
         _endpointUrl,
         JSON.stringify(data),
-        function(data) {
-            console.log(data);
+        async function(data) {
+            await createTable(data);
         }
     );
+}
+
+async function createTable(_data) {
+    var table = "<table border=\"0\">";
+    
+    _data = JSON.parse(_data);
+
+    const dataKeys = Object.keys(_data.data);
+
+    table += "<thead><tr id=\"row-header\">";
+
+    for(const key of ["Officer ID", "Status", "Date created", "Date modified"]) {
+        table += "<th>" + key + "</th>";
+    }
+
+    table += "</tr></thead><tbody>";
+
+    for(const dataRow of dataKeys) {
+        const row = _data.data[dataRow];
+
+        table += "<tr>";
+
+        for(const r of ["currentOfficerId", "status", "dateCreated", "dateModified"]) {
+            table += "<td id=\"col-" + r + "\">" + row[r] + "</td>";
+        }
+
+        table += "</tr>";
+    }
+
+    table += "</tbody></table>";
+
+    $("#processes-waiting-for-me").html(table);
 }

a2704c42f833961b573cdd9ad2c62835ff84a0a3
Lukas Velek <lukas.velek@centrum.cz>
2025-06-11 16:55:26 +0200

    Container deleting update

--- app/constants/JobQueueTypes.php
+++ app/constants/JobQueueTypes.php
@@ -3,12 +3,12 @@
 namespace App\Constants;
 
 class JobQueueTypes extends AConstant {
-    public const DELETE_CONTAINER_INSTANCE = 1;
+    public const DELETE_CONTAINER = 1;
     public const DELETE_CONTAINER_PROCESS_INSTANCE = 2;
 
     public static function toString($key): ?string {
         return match((int)$key) {
-            self::DELETE_CONTAINER_INSTANCE => 'Delete container instance',
+            self::DELETE_CONTAINER => 'Delete container',
             self::DELETE_CONTAINER_PROCESS_INSTANCE => 'Delete container process instance'
         };
     }

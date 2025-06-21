f6a613301305aa3b6a426ea557fd8b7191ab8160
Lukas Velek <lukas.velek@centrum.cz>
2025-06-21 08:45:16 +0200

    Update version.php

--- version.php
+++ version.php
@@ -1,7 +1,7 @@
 <?php
 
-define('APP_VERSION', '1.6');
-define('APP_VERSION_RELEASE_DATE', '06/21/2025');
+define('APP_VERSION', '1.6-dev');
+define('APP_VERSION_RELEASE_DATE', '-');
 define('APP_VERSION_BUILD', '0');
 
 ?>

2914d9c297e9e321a868e18b12376792f7231b1b
Lukas Velek <lukas.velek@centrum.cz>
2025-06-26 13:09:24 +0200

    Current commit update

--- app/core/Configuration.php
+++ app/core/Configuration.php
@@ -25,14 +25,35 @@ class Configuration {
      * Returns current version
      */
     public static function getCurrentVersion(): string {
+        $commit = (APP_VERSION_GITHUB_COMMIT != '-') ? APP_VERSION_GITHUB_COMMIT : (self::getCommit() ?? '-');
+
         $releaseDate = (APP_VERSION_RELEASE_DATE != '-' ? ('_' . APP_VERSION_RELEASE_DATE) : '');
-        $fullVersion = APP_VERSION . '+Build_' . APP_VERSION_BUILD . $releaseDate . '+Commit_' . APP_VERSION_GITHUB_COMMIT;
+        $fullVersion = APP_VERSION . '+Build_' . APP_VERSION_BUILD . $releaseDate . '+Commit_' . $commit;
 
         if(APP_BRANCH == 'PROD') {
             $fullVersion = APP_VERSION . ' ' . $fullVersion;
         }
 
         return $fullVersion;
+    }
+
+    /**
+     * Returns current commit
+     */
+    private static function getCommit(): ?string {
+        $commit = 'ttt';
+
+        if(FileManager::fileExists(APP_ABSOLUTE_DIR . '.git\\FETCH_HEAD')) {
+            $lines = FileManager::loadFileLineByLine(APP_ABSOLUTE_DIR . '.git\\FETCH_HEAD');
+
+            foreach($lines as $line) {
+                if(!str_contains($line, 'not-for-merge')) {
+                    $commit = substr($line, 0, 6);
+                }
+            }
+        }
+
+        return $commit;
     }
 }
 

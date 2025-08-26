f87cff1d323e1e973689ddbae50c6009a59a48b1
Lukas Velek <lukas.velek@centrum.cz>
2025-08-26 14:03:46 +0200

    Application log grid update

--- app/modules/SuperAdminSettingsModule/Presenters/ApplicationLogPresenter.php
+++ app/modules/SuperAdminSettingsModule/Presenters/ApplicationLogPresenter.php
@@ -3,6 +3,10 @@
 namespace App\Modules\SuperAdminSettingsModule;
 
 use App\Constants\ApplicationLogTypes;
+use App\Core\DB\DatabaseRow;
+use App\UI\GridBuilder2\Cell;
+use App\UI\GridBuilder2\Row;
+use App\UI\HTML\HTML;
 
 class ApplicationLogPresenter extends ASuperAdminSettingsPresenter {
     public function __construct() {
@@ -15,10 +19,25 @@ class ApplicationLogPresenter extends ASuperAdminSettingsPresenter {
         $grid = $this->componentFactory->getGridBuilder();
 
         $qb = $this->app->appLogRepository->composeQueryForApplicationLog();
+        $qb->andWhere('type <> ?', [ApplicationLogTypes::STOPWATCH])
+            ->orderBy('dateCreated', 'DESC');
 
         $grid->createDataSourceFromQueryBuilder($qb, 'logId');
 
-        $grid->addColumnText('message', 'Message');
+        $col = $grid->addColumnText('message', 'Message');
+        $col->onRenderColumn[] = function(DatabaseRow $row, Row $_row, Cell $cell, HTML $html, mixed $value) {
+            $el = HTML::el('span');
+
+            $el->title($value);
+
+            if(strlen($value) > 50) {
+                $el->text(substr($value, 0, 50) . '...');
+            } else {
+                $el->text($value);
+            }
+
+            return $el;
+        };
         $grid->addColumnText('method', 'Method');
         $grid->addColumnConst('type', 'Type', ApplicationLogTypes::class);
         $grid->addColumnDatetime('dateCreated', 'Date created');

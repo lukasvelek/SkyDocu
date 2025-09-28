6df1710f6aaf4fc4c098e3d11bada54e89c1e876
Lukas Velek <lukas.velek@centrum.cz>
2025-09-28 13:06:37 +0200

    ContainerUsageStatisticsService bugfix

--- app/services/ContainerUsageStatisticsService.php
+++ app/services/ContainerUsageStatisticsService.php
@@ -148,7 +148,7 @@ class ContainerUsageStatisticsService extends AService {
                 'totalTimeTaken' => $totalTimeTaken
             ];
         } catch(AException|Exception $e) {
-            return 0;
+            return [];
         }
     }
 
@@ -156,6 +156,8 @@ class ContainerUsageStatisticsService extends AService {
         foreach($results as $containerId => $data) {
             $this->logInfo('Saving analysis results for container \'' . $containerId . '\'.');
             foreach($data as $date => $measuredData) {
+                if(empty($measuredData)) continue;
+                
                 $totalSqlQueries = $measuredData['count'];
                 $averageTimeTaken = (float)$measuredData['averageTimeTaken'];
                 $totalTimeTaken = (float)$measuredData['totalTimeTaken'];
